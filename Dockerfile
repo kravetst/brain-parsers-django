25976ab14a1f8b95f42602ba42b1e80ecaef137a
kravetst <taraskravetc28@gmail.com>
2025-12-09 10:58:53 +0200

    Update environment and Docker configuration

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,4 @@ COPY . .
 
 EXPOSE 8000
 
-CMD ["python", "app/manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
+    BASE_DIR = Path(__file__).resolve().parent
+    sys.path.append(str(BASE_DIR / 'app'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brain_parsers_project.settings')
     try:
         from django.core.management import execute_from_command_line
